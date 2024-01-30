c668759a2a3fe0dc8b91ad20280ec99a1837f356
hftamayo <hftamayo@gmail.com>
2024-01-30 10:57:41 -0600

    adding grapql endpoints to the main index

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
-import  express  from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { ApolloServer } from "@apollo/server";
-import {expressMiddleware} from "@apollo/server/express4";
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs, resolvers } from "./graphql";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
 const bootstrapServer = async () => {
-    app.use(cors());
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
 
-    app.get("/", (req, res) => {
-        res.send("Hello World!");
-    });
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/graphql", expressMiddleware(server));
 
-    app.listen({port}, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen({ port }, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    console.log(`GraphQL server running at http://localhost:${port}/graphql`)
+  });
 };
 
 bootstrapServer();
